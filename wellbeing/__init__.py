15f39df3ba23e909519189126f50ac68abc0a375
devnull <devnull@example.com>
2026-02-27 15:54:16 -0500

    Fix wellbeing __init__.py - remove welbeing references

--- wellbeing/__init__.py
+++ wellbeing/__init__.py
@@ -1,13 +1,3 @@
-# Compatibility shim for misnamed package directory 'welbeing'
-# Re-export modules from the existing 'welbeing' package so imports
-# using 'wellbeing' (correct spelling) work without renaming folders.
-
-from importlib import import_module as _import
-
-_import('welbeing.activity_effects')
-_import('welbeing.stress_burnout')
-_import('welbeing.time_budget')
-
 from wellbeing.activity_effects import *
 from wellbeing.stress_burnout import *
 from wellbeing.time_budget import *
